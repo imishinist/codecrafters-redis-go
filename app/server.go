97220af2db9713b1ecf299bfc217c0be458fcc74
Taisuke Miyazaki <imishinist@gmail.com>
2023-01-05 17:47:21 +0900

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -7,17 +7,20 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error write response: ", err.Error())
 		os.Exit(1)
 	}
 }
